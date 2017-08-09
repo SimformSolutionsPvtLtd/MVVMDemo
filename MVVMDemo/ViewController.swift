04a439d1ba5f84524de3875230d3d4b53d40d742
Ankit Panchal <ankit.p@simformsolutions.com>
2017-08-09 11:55:32 +0530

    Initial Commit

---
+++ MVVMDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by Ankit Panchal on 09/08/17.
+//  Copyright © 2017 Ankit Panchal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
